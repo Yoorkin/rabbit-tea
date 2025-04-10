b25417bacf5511787d88bcf3ca3cbacc2c5352db
Yorkin <cyb_yorkin@outlook.com>
2025-04-10 11:08:21 +0800

    add src/example/form

---
+++ src/example/form/server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const app = express();
+const port = 3001;
+
+// Middleware to parse JSON bodies
+app.use(bodyParser.json());
+
+// Start the server
+app.listen(port, () => {
+    console.log(`Server is listening on http://localhost:${port}`);
+});
+
+// Serve example.js
+app.get('/main.js', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../../target/js/release/build/example/form/form.js'));
+});
+
+// Serve index.html
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+    console.log('GET /');
+});
+
+
